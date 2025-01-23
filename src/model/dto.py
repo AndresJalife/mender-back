256403e0224fd450003f335f6dfd2de81ad38917
Andres Jalife <andres.jalife@mercadolibre.com>
2025-01-23 18:50:39 -0300

    enums to dto

--- src/model/dto.py
+++ src/model/dto.py
@@ -2,6 +2,9 @@ from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
 
+from src.enums.Countries import Countries
+from src.enums.user.UserSex import UserSex
+
 
 class User(BaseModel):
     user_id: Optional[int] = None
@@ -9,9 +12,9 @@ class User(BaseModel):
     password: Optional[str] = None
     name: Optional[str] = None
     username: Optional[str] = None
-    country: Optional[str] = None
+    country: Optional[Countries] = None
     new: Optional[bool] = None
-    sex: Optional[str] = None
+    sex: Optional[UserSex] = None
     created_date: Optional[str] = None
     uid: Optional[str] = None
     model_config = ConfigDict(from_attributes=True)
