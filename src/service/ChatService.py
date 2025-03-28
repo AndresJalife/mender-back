e44dc9d1c4a96cb33564af6171057274bfad2f93
Andres Jalife <andres.jalife@mercadolibre.com>
2025-03-28 19:24:26 -0300

    fix bot chat

--- src/service/ChatService.py
+++ src/service/ChatService.py
@@ -1,5 +1,6 @@
 from src import models
 from src.config.database import Database
+from src.model import dto
 from src.service.Logger import logger
 
 
@@ -24,8 +25,8 @@ class ChatService:
 
     def send_message(self, user, message):
         logger.info(f"Sending message from user {user.user_id}")
-        self._save_message(user, message)
-        return self._get_bot_message(user, message)
+        next_order = self._save_message(user, message)
+        return self._get_bot_message(user, message, next_order)
 
     def _save_message(self, user, message):
         last_message = self._get_last_message(user)
@@ -43,10 +44,17 @@ class ChatService:
         self.db.add(next_message)
         self.db.commit()
 
+        return next_message.order + 1
+
     def _get_last_message(self, user):
         return self.db.query(models.ChatHistory).filter(models.ChatHistory.user_id == user.user_id).order_by(
             models.ChatHistory.order.desc()).first()
 
-    def _get_bot_message(self, user, message):
-        pass
+    def _get_bot_message(self, user, message, next_order):
+        # get next bot message, save and return
+        return dto.Message(
+            bot_made=True,
+            order=next_order,
+            message="Hello, I am a bot!"
+         )
 
