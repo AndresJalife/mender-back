966dde52e1bc5d10624749268bab8555ac1710f9
Andres Jalife <andres.jalife@mercadolibre.com>
2025-03-30 22:36:18 -0300

    fix user.id

--- src/service/PostService.py
+++ src/service/PostService.py
@@ -43,54 +43,6 @@ class PostService:
         )
         return posts
 
-    # def get_posts(self, user):
-    #     logger.info(f"Getting posts for user: {user.user_id}")
-    #     return [
-    #         {
-    #             "post_id": 1,
-    #             "entity_id": 802119,
-    #             "entity_type": 'm',
-    #             "entity": {
-    #                 "title": "The Shawshank Redemption",
-    #                 "overview": "Imprisoned in the 1940s for the double murder of his wife and her lover, "
-    #                             "upstanding banker Andy Dufresne begins a new life at the Shawshank prison, "
-    #                             "where he puts his accounting skills to work for an amoral warden. During his"
-    #                             " long stretch in prison, Dufresne comes to be admired by the other inmates -- "
-    #                             "including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
-    #                 "year": 1994,
-    #                 "link": "xyXX8LXiNJ4",
-    #                 "director": "Frank Darabont",
-    #                 "screenplay": "Stephen King",
-    #                 "genres": ["Drama", "Crime"],
-    #                 "rating": 8.7
-    #             },
-    #             "likes": 40,
-    #             "liked": True,
-    #             "seen": False,
-    #             "comments": 3
-    #         },
-    #         {
-    #             "post_id": 2,
-    #             "entity_id": 802114,
-    #             "entity_type": 'm',
-    #             "entity": {
-    #                 "title": "Flow",
-    #                 "overview": "A solitary cat, displaced by a great flood, finds refuge on a boat with various species "
-    #                             "and must navigate the challenges of adapting to a transformed world together.",
-    #                 "year": 2024,
-    #                 "link": "l5zSgSuIDU4",
-    #                 "director": "Gints Zilbalodis",
-    #                 "screenplay": "Matīss Kaža",
-    #                 "genres": ["Animation", "Fantasy", "Adventure"],
-    #                 "rating": 8.3
-    #             },
-    #             "likes": 15,
-    #             "liked": False,
-    #             "seen": True,
-    #             "comments": 0
-    #         }
-    #     ]
-
     def create_post(self, request: dto.Post):
         try:
             db_post = Post(**request.dict())
@@ -107,29 +59,22 @@ class PostService:
             raise HTTPException(status_code=404, detail="El Post no se ha encontrado.")
         return post
 
-    def get_post_by_tmdb_id(self, tmdb_id, entity_type):
-        logger.info(f"Getting post: {tmdb_id}")
-        post = self.db.query(Post).filter((Post.tmdb_id == tmdb_id)).first()
-        if post is None:
-            raise HTTPException(status_code=404, detail="El Post no se ha encontrado.")
-        return post
-
     def like_post(self, post_id, user):
-        logger.info(f"Liking post: {post_id} for user: {user.id}")
+        logger.info(f"Liking post: {post_id} for user: {user.user_id}")
         self.background_tasks.add_task(self._sum_to_post_likes, post_id)
         self.background_tasks.add_task(self._like_user_post, post_id, user)
 
     def comment_post(self, post_id, user, comment):
-        logger.info(f"Commenting post: {post_id} for user: {user.id}")
+        logger.info(f"Commenting post: {post_id} for user: {user.user_id}")
         self.background_tasks.add_task(self._sum_to_post_comments, post_id)
         self.background_tasks.add_task(self._comment_user_post, post_id, user, comment)
 
     def rate_post(self, post_id, user, rate):
-        logger.info(f"Rating post: {post_id} for user: {user.id}")
+        logger.info(f"Rating post: {post_id} for user: {user.user_id}")
         self.background_tasks.add_task(self._rate_user_post, post_id, user, rate.rating)
 
     def see_post(self, post_id, user):
-        logger.info(f"Seeing post: {post_id} for user: {user.id}")
+        logger.info(f"Seeing post: {post_id} for user: {user.user_id}")
         self.background_tasks.add_task(self._see_user_post, post_id, user)
 
     def _sum_to_post_likes(self, post_id):
@@ -153,7 +98,7 @@ class PostService:
         user_post_info = self.db.query(UserPostInfo).filter(UserPostInfo.post_id == post_id).first()
 
         if user_post_info is None:
-            user_post_info = UserPostInfo(user_id=user.id, post_id=post_id)
+            user_post_info = UserPostInfo(user_id=user.user_id, post_id=post_id)
 
         self.db.add(user_post_info)
         return user_post_info
@@ -166,7 +111,7 @@ class PostService:
         self.db.commit()
 
     def _comment_user_post(self, post_id, user, comment):
-        comment = Comments(user_id=user.id, post_id=post_id, comment=comment.comment)
+        comment = Comments(user_id=user.user_id, post_id=post_id, comment=comment.comment)
         self.db.add(comment)
         self.db.commit()
 
